0bef29cdf069256e19c1c93127da9b8138b147e5
Dino <dino.musliu@outlook.com>
2016-06-26 16:13:25 +0200

    Initial Commit

---
+++ oldSchoolMac-textboxWhileFor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oldSchoolMac-textboxWhileFor
+//
+//  Created by Dino Musliu on 26/06/2016.
+//  Copyright © 2016 Dino Musliu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
