e2744147d3f5962584c32db48db00d492ca55c8d
Dino <dinomusliu@Dinos-MacBook-Pro-2.local>
2016-07-22 19:40:34 +0200

    oldSchoolMac iOS

--- oldSchoolMac-textboxWhileFor/ViewController.swift
+++ oldSchoolMac-textboxWhileFor/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblName: UILabel!
+    
+    var phrases = ["Booting from floppy...\n", "Reading from disk...\n", "Updating Registry...", ".........",".............", "Welcome, Dino. It is nice to see you again"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        lblName.text = ""
+        
+        for phrase in phrases {
+            
+            /*
+            var text = lblName.text!
+            text += phrase
+            lblName.text = text
+            */
+            
+            lblName.text! += phrase + "\n"
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
